217377df1f2fd790139ce0a2c241ff30b966b680
rezageramipour <rezageramipour7@gmail.com>
2023-11-27 09:45:14 -0800

    tamrin 3

---
+++ static/assets/js/script.js
@@ -0,0 +1,8 @@
+var userNumber1 = prompt("عدد اول را وارد کنید:", 1);
+var userNumber2 = prompt("عدد دوم را وارد کنید:", 2);
+var userNumber3 = prompt("عدد سوم را وارد کنید:", 3);
+var plas;
+var average;
+plas = +userNumber1 + +userNumber2 + +userNumber3;
+average = plas / 3;
+alert (average);
